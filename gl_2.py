9959f89582d839d134dba041936f6a3eb82aeaf0
andrieu <pierre.andrieu@lri.fr>
2022-09-21 16:53:46 +0200

    test

--- gl_2.py
+++ gl_2.py
@@ -8,14 +8,9 @@ une cellule morte avec exactement 3 voisins vivants devient vivante
 
 class Jdlv:
     def __init__(self, path: str):
-        self.n = 4
-        self.m = 8
-
-
-def test1():
-    jdlv = Jdlv("jdlv.txt")
-    assert jdlv.n == 4
-    assert jdlv.m == 8
-
-test1()
+        with open(path, 'r') as fichier:
+            lignes = fichier.read().split("\n")
+            en_tete = lignes[0].split(" ")
+            self.n = int(en_tete[0])
+            self.m = int(en_tete[1])
 
--- tests.py
+++ tests.py
@@ -1,3 +1,20 @@
+"""
+Une cellule vivante ave moins de 2 voisins vivants meurt 
+Une cellule vivante avec 2 ou 3 voisins survit
+une cellule vivante avec plus de 3 voisins meurt
+une cellule morte avec exactement 3 voisins vivants devient vivante
+
+"""
+
+class Jdlv:
+    def __init__(self, path: str):
+        with open(path, 'r') as fichier:
+            lignes = fichier.read().split("\n")
+            en_tete = lignes[0].split(" ")
+            self.n = int(en_tete[0])
+            self.m = int(en_tete[1])
+
+
 def test1():
     jdlv = Jdlv("jdlv.txt")
     assert jdlv.n == 4
